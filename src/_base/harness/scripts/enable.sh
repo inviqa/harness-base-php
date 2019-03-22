92f3c6c7320955bf13177d3ca195bca63dc6e137
test <dcole@inviqa.com>
2019-03-22 07:46:01 +0000

    refactor enable and fix exit error

--- src/_base/harness/scripts/enable.sh
+++ src/_base/harness/scripts/enable.sh
@@ -2,67 +2,51 @@
 
 main()
 {
-    if [[ "$APP_BUILD" = "dynamic" ]]; then
-        dynamic
+    if [ ! -f .flag-built ]; then
+    
+        passthru docker-compose -p "$NAMESPACE" down
+
+        if [[ "$HAS_ASSETS" = "yes" ]]; then
+            ws assets download
+        fi
+
+        $APP_BUILD
+        touch .flag-built
+
     else
-        static
+        passthru docker-compose -p "$NAMESPACE" up -d
+        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app welcome
+    fi
+
+    if [[ "$APP_BUILD" = "dynamic" && "$USE_DOCKER_SYNC" = "yes" ]]; then
+        passthru docker-sync start
     fi
 }
 
 dynamic()
 {
-    if [ ! -f .flag-built ]; then
+    # we synchronise then stop docker-sync as leaving it running during the build
+    # will often cause it to crash.
 
-        passthru docker-compose -p "$NAMESPACE" down
-
-        [[ "$HAS_ASSETS" = "yes" ]] && ws assets download
-
-        # we synchronise then stop docker-sync as leaving it running during the build
-        # will often cause it to crash.
-
-        if [[ "$USE_DOCKER_SYNC" = "yes" ]]; then
-            passthru docker-sync start
-            passthru docker-sync stop
-        fi
-
-        passthru docker-compose -p "$NAMESPACE" pull
-        passthru docker-compose -p "$NAMESPACE" build --pull
-        passthru docker-compose -p "$NAMESPACE" up -d
-
-        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app build
-        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app init
-
-        touch .flag-built
-
-    else
-
-        passthru docker-compose -p "$NAMESPACE" up -d
-        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app welcome
-        
+    if [[ "$USE_DOCKER_SYNC" = "yes" ]]; then
+        passthru docker-sync start
+        passthru docker-sync stop
     fi
 
-    [[ "$USE_DOCKER_SYNC" = "yes" ]] && passthru docker-sync start
+    passthru docker-compose -p "$NAMESPACE" pull
+    passthru docker-compose -p "$NAMESPACE" build --pull
+    passthru docker-compose -p "$NAMESPACE" up -d
+
+    passthru docker-compose -p "$NAMESPACE" exec -T -u build console app build
+    passthru docker-compose -p "$NAMESPACE" exec -T -u build console app init
 }
 
 static()
 {
-    if [ ! -f .flag-built ]; then
-
-        [[ "$HAS_ASSETS" = "yes" ]] && ws assets download
-
-        ws app build
-        
-        passthru docker-compose -p "$NAMESPACE" up -d
-        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app init
-
-        touch .flag-built
-
-    else
-
-        passthru docker-compose -p "$NAMESPACE" up -d
-        passthru docker-compose -p "$NAMESPACE" exec -T -u build console app welcome
-
-    fi
+    ws app build
+    
+    passthru docker-compose -p "$NAMESPACE" up -d
+    passthru docker-compose -p "$NAMESPACE" exec -T -u build console app init
 }
 
 main
