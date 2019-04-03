1e9f946e767da1e2497cdf78efc08ddf53e71e72
dcole-inviqa <dcole@inviqa.com>
2019-04-03 10:05:36 +0100

    use the same composer install command regardless of app mode

--- src/_base/docker/image/console/root/lib/task/composer/install.sh
+++ src/_base/docker/image/console/root/lib/task/composer/install.sh
@@ -2,9 +2,5 @@
 
 function task_composer_install()
 {
-    if [ "$APP_MODE" = "production" ]; then
-        passthru "composer install --no-interaction --optimize-autoloader"
-    else
-        passthru "composer install --no-interaction"
-    fi
+    passthru "composer install --no-interaction"
 }
