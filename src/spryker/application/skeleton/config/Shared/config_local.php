e7c30c9156911a889d1899693486fbf8a42831a5
Hardik Gajjar <hgajjar@inviqa.com>
2019-05-09 17:12:09 +0530

    Fix some errors detected by php code sniffer

--- src/spryker/application/skeleton/config/Shared/config_local.php
+++ src/spryker/application/skeleton/config/Shared/config_local.php
@@ -1,20 +1,20 @@
 <?php
+
+use Monolog\Logger;
 use Spryker\Shared\Application\ApplicationConstants;
-use Spryker\Shared\Collector\CollectorConstants;
 use Spryker\Shared\Customer\CustomerConstants;
+use Spryker\Shared\GlueApplication\GlueApplicationConstants;
+use Spryker\Shared\Log\LogConstants;
 use Spryker\Shared\Newsletter\NewsletterConstants;
 use Spryker\Shared\ProductManagement\ProductManagementConstants;
 use Spryker\Shared\Propel\PropelConstants;
+use Spryker\Shared\PropelQueryBuilder\PropelQueryBuilderConstants;
+use Spryker\Shared\RabbitMq\RabbitMqEnv;
 use Spryker\Shared\Search\SearchConstants;
 use Spryker\Shared\Session\SessionConstants;
 use Spryker\Shared\Setup\SetupConstants;
 use Spryker\Shared\Storage\StorageConstants;
-use Spryker\Shared\PropelQueryBuilder\PropelQueryBuilderConstants;
 use Spryker\Shared\ZedRequest\ZedRequestConstants;
-use Spryker\Shared\Log\LogConstants;
-use Monolog\Logger;
-use Spryker\Shared\RabbitMq\RabbitMqEnv;
-use Spryker\Shared\GlueApplication\GlueApplicationConstants;
 
 $config[ProductManagementConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
 $config[NewsletterConstants::BASE_URL_YVES] = $config[ApplicationConstants::BASE_URL_YVES];
@@ -53,14 +53,14 @@ $config[PropelConstants::ZED_DB_ENGINE]
     = $config[PropelQueryBuilderConstants::ZED_DB_ENGINE]
     = $config[PropelConstants::ZED_DB_ENGINE_PGSQL];
 $config[PropelConstants::USE_SUDO_TO_MANAGE_DATABASE] = false;
-/** Elasticsearch  */
+/** Elasticsearch */
 $config[ApplicationConstants::ELASTICA_PARAMETER__HOST] = getenv('ELASTICSEARCH_HOST');
 $config[ApplicationConstants::ELASTICA_PARAMETER__PORT] = getenv('ELASTICSEARCH_PORT');
 unset($config[SearchConstants::ELASTICA_PARAMETER__AUTH_HEADER]);
 $ELASTICA_PARAMETER__EXTRA = [
     // TODO: add here aws region and other extra config you need, for example
     'aws_region' => 'eu-central-1',
-    'transport' => 'AwsAuthV4'
+    'transport' => 'AwsAuthV4',
 ];
 $config[ApplicationConstants::ELASTICA_PARAMETER__EXTRA] = $ELASTICA_PARAMETER__EXTRA;
 $config[SearchConstants::ELASTICA_PARAMETER__EXTRA] = $ELASTICA_PARAMETER__EXTRA;
@@ -88,34 +88,34 @@ $config[RabbitMqEnv::RABBITMQ_API_USERNAME] = getenv('RABBITMQ_USER');
 $config[RabbitMqEnv::RABBITMQ_API_PASSWORD] = getenv('RABBITMQ_PASSWORD');
 $config[RabbitMqEnv::RABBITMQ_API_VIRTUAL_HOST] = getenv('RABBITMQ_VHOST');
 $config[RabbitMqEnv::RABBITMQ_CONNECTIONS] = [
-'DE' => [
-RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'DE-connection',
-RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
-RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
-RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
-RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
-RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST'),
-RabbitMqEnv::RABBITMQ_STORE_NAMES => ['DE'],
-RabbitMqEnv::RABBITMQ_DEFAULT_CONNECTION => true,
-],
-'AT' => [
-RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'AT-connection',
-RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
-RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
-RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
-RabbitMqEnv::RABBITMQ_USERNAME =>  getenv('RABBITMQ_USER'),
-RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST'),   #should be another one
-RabbitMqEnv::RABBITMQ_STORE_NAMES => ['AT'],
-],
-'US' => [
-RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'US-connection',
-RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
-RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
-RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
-RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
-RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST'),  #should be another one
-RabbitMqEnv::RABBITMQ_STORE_NAMES => ['US'],
-],
+    'DE' => [
+        RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'DE-connection',
+        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
+        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
+        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
+        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
+        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST'),
+        RabbitMqEnv::RABBITMQ_STORE_NAMES => ['DE'],
+        RabbitMqEnv::RABBITMQ_DEFAULT_CONNECTION => true,
+    ],
+    'AT' => [
+        RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'AT-connection',
+        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
+        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
+        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
+        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
+        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST'),   #should be another one
+        RabbitMqEnv::RABBITMQ_STORE_NAMES => ['AT'],
+    ],
+    'US' => [
+        RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'US-connection',
+        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
+        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
+        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
+        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
+        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST'),  #should be another one
+        RabbitMqEnv::RABBITMQ_STORE_NAMES => ['US'],
+    ],
 ];
 /** Jenkins **/
 $config[SetupConstants::JENKINS_BASE_URL] = 'http://' . getenv('JENKINS_HOST') . ':' . getenv('JENKINS_PORT') . '/';
