a0b5d1f509bf996f64359e427f934bb9a267439d
Hardik Gajjar <hgajjar@inviqa.com>
2019-06-25 17:46:14 +0530

    Handle empty array case

--- src/_base/docker/image/php-fpm/root/entrypoint.sh
+++ src/_base/docker/image/php-fpm/root/entrypoint.sh
@@ -3,9 +3,14 @@
 docker_entrypoint()
 {
     # run any command required to be executed at docker startup
+{% if @('php-fpm.entrypoint.steps') is empty %}
+    :
+{% else %}
     {% for step in @('php-fpm.entrypoint.steps') -%}
     {{ step|raw }}
     {% endfor %}
+{% endif %}
+
 }
 
 docker_entrypoint
