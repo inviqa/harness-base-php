42a9f745eabb1a71739d5f57d64ce77252d488f4
Hardik Gajjar <hgajjar@inviqa.com>
2019-06-25 17:35:40 +0530

    Allow running a command at entrypoint in php-fpm image

--- src/_base/docker/image/php-fpm/root/entrypoint.sh
+++ src/_base/docker/image/php-fpm/root/entrypoint.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
+docker_entrypoint()
+{
+    # run any command required to be executed at docker startup
+    {% for step in @('php-fpm.entrypoint.steps') -%}
+    {{ step|raw }}
+    {% endfor %}
+}
+
+docker_entrypoint
+
 # run
 exec supervisord -c /etc/supervisor/supervisord.conf -n
