df413b1279b1352cee76bd4450ffdb563f6ccb04
Hardik Gajjar <hgajjar@inviqa.com>
2019-07-31 11:02:25 +0530

    MISC: Remove jenkins slave node before reattaching new one with same name

--- src/spryker/docker/image/jenkins-runner/root/lib/task/jenkins/setup.sh
+++ src/spryker/docker/image/jenkins-runner/root/lib/task/jenkins/setup.sh
@@ -11,8 +11,14 @@ function task_jenkins_setup()
 
     (java -jar /usr/local/bin/jenkins-cli.jar -s "$JENKINS_URL" get-node "$JENKINS_RUNNER_NAME" 2>&1 || true ) > /tmp/jenkins.node
 
-    if grep ERROR: /tmp/jenkins.node >/dev/null; then
-        echo -e "Registering jenkins runner $JENKINS_RUNNER_NAME at master"
+    if ! grep ERROR: /tmp/jenkins.node >/dev/null; then
+        # In case of runner recreation, jenkins does not allow re-attaching new runner with same node name,
+        # may be due to IP change, so here we need to remove it first if already exists, and then re-register
+        echo -e "Removing old jenkins runner $JENKINS_RUNNER_NAME:"
+        java -jar /usr/local/bin/jenkins-cli.jar -s "$JENKINS_URL" delete-node "$JENKINS_RUNNER_NAME"
+    fi
+
+    echo -e "Registering jenkins runner $JENKINS_RUNNER_NAME at master"
         # shellcheck disable=SC2154
         cat <<EOF | java -jar /usr/local/bin/jenkins-cli.jar -s "$JENKINS_URL" create-node "$JENKINS_RUNNER_NAME"
             <slave>
@@ -27,10 +33,6 @@ function task_jenkins_setup()
               <nodeProperties/>
             </slave>
 EOF
-    else
-        echo -e "Already registered as jenkins runner $JENKINS_RUNNER_NAME:"
-        cat /tmp/jenkins.node
-    fi
 
     # link php where it is expected by cron.conf
     ln -s /usr/local/bin/php /usr/bin/php
