dc983f75b7a1dc904884598887150d0840a7fd54
Gavin Foster <gavin.foster@inviqa.com>
2019-11-28 11:20:14 +0000

    [DEVOPS-1115] just the build changes

--- src/spryker/docker/image/console/root/lib/task/spryker/init.sh
+++ src/spryker/docker/image/console/root/lib/task/spryker/init.sh
@@ -2,10 +2,8 @@
 
 function task_spryker_init()
 {
+    run "vendor/bin/console propel:install -o"
     passthru "vendor/bin/install -r docker -s queue-flush"
-
-    task "assets:apply" # calls task "install" when database is not applied
-
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s queue-worker"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s jenkins-up"
 }
