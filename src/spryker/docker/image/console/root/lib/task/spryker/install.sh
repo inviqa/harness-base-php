738ae17fc0c13b79d0d5c427d2a1bb9e8def1955
Gavin Foster <gavin.foster@inviqa.com>
2019-11-28 11:16:46 +0000

    [DEVOPS-1115] just the build changes

--- src/spryker/docker/image/console/root/lib/task/spryker/install.sh
+++ src/spryker/docker/image/console/root/lib/task/spryker/install.sh
@@ -2,6 +2,7 @@
 
 function task_spryker_install()
 {
+    passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -x frontend"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s database-migrate"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s demodata"
 }
