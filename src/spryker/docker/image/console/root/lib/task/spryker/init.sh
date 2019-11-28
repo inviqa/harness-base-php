5281af6dc05479886800bc3cfe001232e98d167d
Gavin Foster <gavin.foster@inviqa.com>
2019-11-28 13:34:54 +0000

    [DEVOPS-1115] just the build changes

--- src/spryker/docker/image/console/root/lib/task/spryker/init.sh
+++ src/spryker/docker/image/console/root/lib/task/spryker/init.sh
@@ -2,7 +2,6 @@
 
 function task_spryker_init()
 {
-    run "vendor/bin/console propel:install -o"
     passthru "vendor/bin/install -r docker -s queue-flush"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s queue-worker"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s jenkins-up"
