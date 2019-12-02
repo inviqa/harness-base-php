465acc3d0c910721cc3de4a6f61e1746b8ec298f
Gavin Foster <gavin.foster@inviqa.com>
2019-12-02 12:08:17 +0000

    [DEVOPS-1115] make demodata load optional

--- src/spryker/docker/image/console/root/lib/task/spryker/install.sh
+++ src/spryker/docker/image/console/root/lib/task/spryker/install.sh
@@ -2,7 +2,5 @@
 
 function task_spryker_install()
 {
-    passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -x frontend"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s database-migrate"
-    passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s demodata"
 }
