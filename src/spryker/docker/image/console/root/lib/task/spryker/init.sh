da8ea43185af0aba95a74c8faccecaaa88ebf663
Gavin Foster <gavin.foster@inviqa.com>
2019-12-02 12:27:56 +0000

    [DEVOPS-1115] match H`s spryker install step order

--- src/spryker/docker/image/console/root/lib/task/spryker/init.sh
+++ src/spryker/docker/image/console/root/lib/task/spryker/init.sh
@@ -2,6 +2,7 @@
 
 function task_spryker_init()
 {
+    run "vendor/bin/console propel:install -o"
     passthru "vendor/bin/install -r docker -s queue-flush"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s queue-worker"
     passthru "PGPASSWORD=$DB_PASS vendor/bin/install -r docker -s jenkins-up"
