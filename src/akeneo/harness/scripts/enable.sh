3e22b7df089414ed24bf869c055a4aed988c0b75
Gavin Foster <47179450+g-foster2@users.noreply.github.com>
2020-01-28 09:25:22 +0000

    Akeneo - split job-queue-consumer from php fpm container (#206)
    * initial changes to support division of akeneo php-fpm container
    
    * initial changes to support division of akeneo php-fpm container
    
    * changes to support division of php-fpm container and configuration of N job queue consumer daemons
    
    * changes to support division of php-fpm container and configuration of N job queue consumer daemons
    
    * changes to support division of php-fpm container and configuration of N job queue consumer daemons
    
    * changes to support division of php-fpm container and configuration of N job queue consumer daemons
    
    Co-authored-by: g-foster <gavin.foster@inviqa.com>

---
+++ src/akeneo/harness/scripts/enable.sh
@@ -0,0 +1,59 @@
+#!/usr/bin/env bash
+
+main()
+{
+    passthru ws networks external
+    if [ ! -f .my127ws/.flag-built ]; then
+
+        passthru docker-compose down
+
+        if [[ "$HAS_ASSETS" = "yes" ]]; then
+            ws assets download
+        fi
+
+        $APP_BUILD
+        touch .my127ws/.flag-built
+
+    else
+        passthru docker-compose up -d
+        passthru docker-compose exec -T -u build console app welcome
+    fi
+
+    if [[ "$APP_BUILD" = "dynamic" && "$USE_DOCKER_SYNC" = "yes" ]]; then
+        passthru docker-sync start
+    fi
+}
+
+dynamic()
+{
+    # we synchronise then stop docker-sync as leaving it running during the build
+    # will often cause it to crash.
+
+    if [[ "$USE_DOCKER_SYNC" = "yes" ]]; then
+        passthru gem install docker-sync --no-document
+        passthru docker-sync start
+        passthru docker-sync stop
+    fi
+
+    passthru "docker-compose config --services | grep -v php-fpm | xargs docker-compose pull"
+    passthru "docker-compose config --services | grep -v job-queue-consumer | xargs docker-compose build --pull"
+    passthru docker-compose build job-queue-consumer
+
+    passthru "docker-compose config --services | grep -v job-queue-consumer | xargs docker-compose up -d"
+
+    passthru docker-compose exec -T -u build console app build
+    passthru docker-compose exec -T -u build console app init
+
+    passthru docker-compose up -d job-queue-consumer
+}
+
+static()
+{
+    ws app build
+
+    passthru "docker-compose config --services | grep -v job-queue-consumer | xargs docker-compose up -d"
+    passthru docker-compose exec -T -u build console app init
+    passthru docker-compose up -d job-queue-consumer
+}
+
+main
