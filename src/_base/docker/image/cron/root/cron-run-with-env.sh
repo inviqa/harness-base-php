1b83acfb765472fee4ef9111cbce1c43b709c7cc
Gavin Foster <gavin.foster@inviqa.com>
2020-02-10 15:34:03 +0000

    Fix so that cron script works with php commands

--- src/_base/docker/image/cron/root/cron-run-with-env.sh
+++ src/_base/docker/image/cron/root/cron-run-with-env.sh
@@ -3,4 +3,4 @@ IFS=$'\n'
 env_vars="$(cat /app/env.sh)"
 env_vars_single_line="$(cat /app/env.sh | sed 's/=\(.*\)/="\1"/' | paste -s -d" " -)"
 script_args="$@"
-/usr/bin/env - "$env_vars" bash -c "sudo $env_vars_single_line -u www-data $script_args"
+/usr/bin/env - "$env_vars" bash -c "sudo $env_vars_single_line -u www-data $script_args" > /proc/1/fd/1 2> /proc/1/fd/2
