68597a7493e49b0f72cdef4b8bbb9287876c1417
Kieren Evans <kierenevans@users.noreply.github.com>
2020-05-04 12:40:06 +0300

    Avoid copying assets to php-fpm from console (#284)

--- src/_base/harness/scripts/destroy.sh
+++ src/_base/harness/scripts/destroy.sh
@@ -11,6 +11,7 @@ elif [[ "$USE_MUTAGEN" = "yes" ]]; then
 fi
 
 if [[ "$APP_BUILD" = "static" ]]; then
+    run "docker images --filter=since='${DOCKER_REPOSITORY}:${APP_VERSION}-console' -q | xargs --no-run-if-empty docker image rm --force"
     run "docker images --filter=reference='${DOCKER_REPOSITORY}:${APP_VERSION}-*' -q | xargs --no-run-if-empty docker image rm --force"
 fi
 
