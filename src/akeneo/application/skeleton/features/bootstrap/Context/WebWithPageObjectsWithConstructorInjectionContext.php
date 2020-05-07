c80b9f3453756602ab444ef5ac8c1ac59f20265b
Kieren Evans <kevans@inviqa.com>
2020-05-07 15:46:14 +0300

    Modernise behat based on magento2 framework (thanks Tibor!)

---
+++ src/akeneo/application/skeleton/features/bootstrap/Context/WebWithPageObjectsWithConstructorInjectionContext.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Inviqa\Acceptance\Context;
+
+use Behat\Behat\Context\Context;
+use Inviqa\Acceptance\Page\HomePage;
+use Inviqa\Acceptance\Page\LoginPage;
+use PHPUnit\Framework\Assert;
+
+class WebWithPageObjectsWithConstructorInjectionContext implements Context
+{
+    /** @var HomePage */
+    private $homePage;
+    /** @var LoginPage */
+    private $loginPage;
+
+    public function __construct(HomePage $homePage, LoginPage $loginPage)
+    {
+        // homepage was injected through the constructor by the page object extension
+        // see documentation here: https://behat-page-object-extension.readthedocs.io/en/stable/guide/working_with_page_objects.html#injecting-page-objects-into-a-context-file
+        $this->homePage = $homePage;
+    }
+
+    /**
+     * @When I visit the home page
+     */
+    public function iVisitTheHomePage()
+    {
+        $this->homePage->open();
+    }
+
+    /**
+     * @Then I should see the home page
+     */
+    public function iShouldSeeTheHomePage()
+    {
+        Assert::assertTrue($this->homePage->isOpen());
+    }
+
+    /**
+     * @Then I should see the login page
+     */
+    public function iShouldSeeTheLoginPage()
+    {
+        Assert::assertTrue($this->loginPage->isOpen());
+    }
+}
