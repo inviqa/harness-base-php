e82f81ea0349eab09d0ef3644de2f6b10f3100a6
Kieren Evans <kevans@inviqa.com>
2020-06-23 18:22:07 +0300

    Install composer in the php-fpm image for drupal8

---
+++ src/drupal8/docker/image/php-fpm/root/usr/bin/install_composer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+# From https://github.com/my127/docker-php/blob/4928447b1ca6302de1ae6691b4251cca34506acb/console.Dockerfile#L51-L70
+curl --silent --fail --location --retry 3 --output /tmp/installer.php --url https://raw.githubusercontent.com/composer/getcomposer.org/b5a70434f0f582468a7824418553defaf67d5299/web/installer \
+ && php -r " \
+    \$signature = 'c5b9b6d368201a9db6f74e2611495f369991b72d9c8cbd3ffbc63edff210eb73d46ffbfce88669ad33695ef77dc76976'; \
+    \$hash = hash('sha384', file_get_contents('/tmp/installer.php')); \
+    if (!hash_equals(\$signature, \$hash)) { \
+        unlink('/tmp/installer.php'); \
+        echo 'Integrity check failed, installer is either corrupt or worse.' . PHP_EOL; \
+        exit(1); \
+    }" \
+ && php /tmp/installer.php --no-ansi --install-dir=/usr/bin --filename=composer --version=1.9.3
+
+composer global require hirak/prestissimo
