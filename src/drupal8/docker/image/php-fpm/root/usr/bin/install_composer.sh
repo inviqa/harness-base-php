e447e74dbcf36c9824ff426daf61a17e846e4834
Kieren Evans <kevans@inviqa.com>
2020-06-24 11:32:38 +0300

    Install git and unzip

--- src/drupal8/docker/image/php-fpm/root/usr/bin/install_composer.sh
+++ src/drupal8/docker/image/php-fpm/root/usr/bin/install_composer.sh
@@ -2,6 +2,15 @@
 
 set -e -o pipefail
 
+apt-get update -qq \
+ && DEBIAN_FRONTEND=noninteractive apt-get -qq -y --no-install-recommends install \
+   curl \
+   git \
+   unzip \
+ && apt-get auto-remove -qq -y \
+ && apt-get clean \
+ && rm -rf /var/lib/apt/lists/*
+
 # From https://github.com/my127/docker-php/blob/4928447b1ca6302de1ae6691b4251cca34506acb/console.Dockerfile#L51-L70
 curl --silent --fail --location --retry 3 --output /tmp/installer.php --url https://raw.githubusercontent.com/composer/getcomposer.org/b5a70434f0f582468a7824418553defaf67d5299/web/installer \
  && php -r " \
