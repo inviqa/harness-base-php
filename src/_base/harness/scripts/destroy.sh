331c13d0b55f3ef4e4da0e101a5ee12ab0c9fd8d
Kieren Evans <kierenevans@users.noreply.github.com>
2020-07-28 17:20:53 +0300

    Remove spryker console dev image (#340)

--- src/_base/harness/scripts/destroy.sh
+++ src/_base/harness/scripts/destroy.sh
@@ -13,6 +13,7 @@ fi
 if [[ "$APP_BUILD" = "static" ]]; then
     run "docker images --filter=since='${DOCKER_REPOSITORY}:${APP_VERSION}-console' -q | xargs --no-run-if-empty docker image rm --force"
     run "docker images --filter=reference='${DOCKER_REPOSITORY}:${APP_VERSION}-*' -q | xargs --no-run-if-empty docker image rm --force"
+    run "docker images --filter=reference='${NAMESPACE}-*:dev' -q | xargs --no-run-if-empty docker image rm --force"
 fi
 
 run rm -f .my127ws/.flag-built
