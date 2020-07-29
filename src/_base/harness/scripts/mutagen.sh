848503a4be19ad89fb4b13772052f0c48916ae6e
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2020-07-29 10:25:28 +0100

    Use mutagen.yml config to manage mutagen docker containers (#336)
    * Use mutagen.yml config to manage mutagen docker containers
    
    Allows projects to set up additional mutagen sync volumes
    
    * Drive docker-compose volume config from mutagen volumes
    
    * Render mutagen.yml first so docker-compose.yml can reference it
    
    * Remove array joining from get_mutagen_volume_names()
    
    It can't be removed from all functions yet as expression language seems to have more limited functions than twig
    
    Co-authored-by: Kieren Evans <kevans@inviqa.com>

--- src/_base/harness/scripts/mutagen.sh
+++ src/_base/harness/scripts/mutagen.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o nounset
+set -o pipefail
+
 if [ "$#" -ne 1 ]; then
     echo "This script supports only one parameter"
     exit 1
@@ -7,6 +11,9 @@ fi
 
 COMMAND="$1"
 PATH="$PATH:./.my127ws/utilities/mutagen/"
+
+# shellcheck disable=SC2206
+CONTAINER_NAMES=($CONTAINER_NAMES)
 
 install_mutagen()
 {
@@ -32,10 +39,22 @@ install_mutagen()
 
 setup_sync_container()
 {
-    if [[ "$(docker ps -a -f "name=${NAMESPACE}-sync" --format '{{.Names}}')" == "${NAMESPACE}-sync" ]]; then
-      passthru docker rm -f "${NAMESPACE}-sync"
-    fi
-    passthru docker run -d --name "${NAMESPACE}-sync" -v "${NAMESPACE}-sync":/app alpine:latest tail -f /dev/null
+    local CONTAINER_NAME
+    local CONTAINER_VOLUME_MAPPINGS
+    local line
+    for CONTAINER_NAME in "${CONTAINER_NAMES[@]}"; do
+        if [[ "$(docker ps -a -f "name=${CONTAINER_NAME}" --format '{{.Names}}')" == "${CONTAINER_NAME}" ]]; then
+            passthru docker rm -f "${CONTAINER_NAME}"
+        fi
+
+        CONTAINER_VOLUME_MAPPINGS=()
+        while IFS= read -r line; do
+            CONTAINER_VOLUME_MAPPINGS+=("$line")
+        done < <(echo "${VOLUME_MAPPINGS}" | grep "^${CONTAINER_NAME}" | cut -d ':' -f2- )
+
+        # shellcheck disable=SC2046
+        passthru docker run -d --name "${CONTAINER_NAME}" $(printf -- '-v %q ' "${CONTAINER_VOLUME_MAPPINGS[@]}") alpine:latest tail -f /dev/null
+    done
 }
 
 start_mutagen_daemon()
@@ -57,19 +76,19 @@ start()
 stop()
 {
     passthru mutagen project terminate
-    passthru docker rm -f "${NAMESPACE}-sync"
+    passthru docker rm -f "${CONTAINER_NAMES[@]}"
 }
 
 pause()
 {
     passthru mutagen project pause
-    passthru docker stop "${NAMESPACE}-sync"
+    passthru docker stop "${CONTAINER_NAMES[@]}"
 }
 
 resume()
 {
     start_mutagen_daemon
-    passthru docker start "${NAMESPACE}-sync"
+    passthru docker start "${CONTAINER_NAMES[@]}"
     passthru mutagen project resume
 }
 
