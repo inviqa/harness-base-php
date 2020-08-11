e519dbe0af65ac774aa8844d105c28d2416b4da3
Kieren Evans <kevans@inviqa.com>
2020-08-11 20:42:48 +0300

    Run a generic task for production quality checks. Move php -l check there.

--- src/_base/docker/image/console/root/lib/task/phpstan.sh
+++ src/_base/docker/image/console/root/lib/task/phpstan.sh
@@ -6,21 +6,17 @@ function task_phpstan()
     phpstan_version="$(find_phpstan_version)"
 
     echo "Using phpstan v$phpstan_version"
-    mkdir -p /app/bin
-    if [ ! -f "/app/bin/phpstan-$phpstan_version.phar" ]; then
-        curl -o "/app/bin/phpstan-$phpstan_version.phar" -L -f -sS "https://github.com/phpstan/phpstan/releases/download/$phpstan_version/phpstan.phar"
-    fi
-    cd /app || return 1
-    php "/app/bin/phpstan-$phpstan_version.phar" analyse
+    composer global require "phpstan/phpstan:$phpstan_version"
+    composer global exec -v -- phpstan analyse -c /app/phpstan.neon
 }
 
 function find_phpstan_version()
 (
-    local phpstan_version="0.12.30"
+    local phpstan_version="0.12.37"
     local project_phpstan_version=""
     set +e
     if [ -f /app/composer.lock ]; then
-        project_phpstan_version="$(jq -r '(."packages-dev"[] | select( .name == "phpstan/phpstan" )).version' < /app/composer.lock)"
+        project_phpstan_version="$(jq -r '."packages" + ."packages-dev" | map(select( .name == "phpstan/phpstan" )) | .[].version' /app/composer.lock)"
         if [ -n "$project_phpstan_version" ]; then
           phpstan_version="$project_phpstan_version"
         fi
