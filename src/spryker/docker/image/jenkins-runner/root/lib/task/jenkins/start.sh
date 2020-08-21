51f5f1a70a411a3426dac9ee08cb420aa3a96cdd
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2020-08-21 14:34:44 +0100

    Make the Spryker jenkins runner single executor, multi containers (#373)
    * Make the Spryker jenkins runner single executor, multi containers
    
    It's much easier to ensure memory limits per container than per executor
    
    Current settings and default memory limits cause OOM errors
    
    * Unregister runner when terminating the container
    
    Also move the registration into the start
    
    * Use native colon separator for commands and deprecate wrapper
    
    * Fix shellcheck and use local vars
    
    * Fix silenced check bug in heredoc and use best practice variable refs
    
    $Always being interpreted as as variable

--- src/spryker/docker/image/jenkins-runner/root/lib/task/jenkins/start.sh
+++ src/spryker/docker/image/jenkins-runner/root/lib/task/jenkins/start.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
 
-# start jenkins runner
+function _trap_jenkins_exit()
+{
+    echo 'Terminating jenkins agent' >&2
+    local CHILD_PIDS
+    mapfile -t CHILD_PIDS < <(jobs -p)
+    kill -TERM "${CHILD_PIDS[@]}"
+    wait
+    app jenkins unregister
+}
+
 function task_jenkins_start()
 {
+    local -r JENKINS_RUNNER_NAME=$(hostname)
+
+    trap _trap_jenkins_exit EXIT
+
+    app jenkins:register
+
     java -jar /usr/local/bin/jenkins-cli.jar -s "$JENKINS_URL" offline-node ""
-    java -jar /usr/local/bin/jenkins-slave.jar -jnlpUrl "$JENKINS_URL/computer/$JENKINS_RUNNER_NAME/slave-agent.jnlp"
+    java -jar /usr/local/bin/jenkins-slave.jar -jnlpUrl "$JENKINS_URL/computer/$JENKINS_RUNNER_NAME/slave-agent.jnlp" &
+
+    wait
 }
