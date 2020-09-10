ac67dee67fa44b406d9d4543d4e84af72e7b0dce
Kieren Evans <kevans@inviqa.com>
2020-09-10 19:31:43 +0300

    Back to yaml routing

--- src/symfony/application/skeleton/src/Controller/ExampleController.php
+++ src/symfony/application/skeleton/src/Controller/ExampleController.php
@@ -3,13 +3,9 @@
 namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Routing\Annotation\Route;
 
 class ExampleController
 {
-    /**
-     * @Route("/")
-     */
     public function homepage(): Response
     {
         return new Response(
