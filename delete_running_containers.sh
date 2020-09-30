8ff9a40d6571a46119a3da1482173636a3ea7142
Kieren Evans <kierenevans@users.noreply.github.com>
2020-09-30 12:22:57 +0300

    Test using 3 Jenkins workers (#428)
    * Test on 5 nodes
    
    * Avoid re-assigning to new nodes
    
    * Combine spryker
    
    * Missed akeneo
    
    * More stages
    
    * Rebalance builds
    
    * Abort parallel builds fast
    
    * Avoid being explicit
    
    * Revert "Avoid being explicit"
    
    This reverts commit b3ca657adc0a6a054663eab93cb21a12edd8ddd8.
    
    * Extra cleanup for docker containers
    
    * Encapsulate in script
    
    * Add some output
    
    * Missed one

---
+++ delete_running_containers.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e -o pipefail
+
+if [ -z "$BUILD_ID" ]; then
+  echo "Skipping as not in Jenkins"
+  exit 1
+fi
+
+echo "Checking running containers:"
+if docker ps | grep -v my127ws | grep -v CONTAINER | grep '^.*$'; then
+  echo "Stopping containers:"
+  docker ps | grep -v my127ws | grep -v CONTAINER | cut -d" " -f1 | xargs --no-run-if-empty docker kill
+else
+  echo -n "No extra containers"
+fi
+echo
+echo "Checking stopped containers:"
+if docker ps -a | grep -v my127ws | grep -v CONTAINER | grep '^.*$'; then
+  echo "Deleting containers:"
+  docker ps -a | grep -v my127ws | grep -v CONTAINER | cut -d" " -f1 | xargs --no-run-if-empty docker rm -f -v
+else
+  echo -n "No extra containers"
+fi
+echo
