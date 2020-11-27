7a3af3a177656a0bbb2001b97bf2f494913de2cf
Kieren Evans <kevans@inviqa.com>
2020-11-27 16:46:45 +0200

    Clean opcache file cache after vendor/composer/ changes

---
+++ src/_base/docker/image/console/root/lib/task/composer/autoload.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+function task_composer_autoload()
+{
+    passthru composer dump-autoload --optimize --classmap-authoritative
+    run rm -rf /tmp/php-file-cache/*/app/vendor/composer || true
+}
