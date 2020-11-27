ce4b88573ec135d25ad7c6324dc9d0ee8773abc2
Kieren Evans <kevans@inviqa.com>
2020-11-27 08:43:26 +0200

    Fix magento2 not finding generated code

--- src/magento2/docker/image/console/root/lib/task/composer/development_dependencies.sh
+++ src/magento2/docker/image/console/root/lib/task/composer/development_dependencies.sh
@@ -3,7 +3,7 @@
 function task_composer_development_dependencies()
 {
     passthru composer install --no-interaction --optimize-autoloader
-    passthru magento setup:upgrade --keep-generated
+    passthru magento setup:upgrade
     passthru magento setup:di:compile
     passthru composer dump-autoload --optimize --classmap-authoritative
 }
