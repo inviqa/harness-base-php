b169ab3673f2e557047a213afff5433fc7582aaa
Kieren Evans <kevans@inviqa.com>
2020-11-27 18:40:00 +0200

    Clear autoload.php too as it contains a random class name

--- src/_base/docker/image/console/root/lib/task/composer/autoload.sh
+++ src/_base/docker/image/console/root/lib/task/composer/autoload.sh
@@ -2,5 +2,5 @@
 function task_composer_autoload()
 {
     passthru composer dump-autoload --optimize --classmap-authoritative
-    run rm -rf /tmp/php-file-cache/*/app/vendor/composer || true
+    run rm -rf /tmp/php-file-cache/*/app/vendor/composer /tmp/php-file-cache/*/app/vendor/autoload.php* || true
 }
--- src/magento2/docker/image/console/root/lib/task/composer/development_dependencies.sh
+++ src/magento2/docker/image/console/root/lib/task/composer/development_dependencies.sh
@@ -3,7 +3,7 @@
 function task_composer_development_dependencies()
 {
     passthru php -d opcache.file_cache_only=0 /usr/bin/composer install --no-interaction
-    run rm -rf /tmp/php-file-cache/*/app/vendor/composer || true
+    run rm -rf /tmp/php-file-cache/*/app/vendor/composer /tmp/php-file-cache/*/app/vendor/autoload.php* || true
     passthru magento setup:upgrade --keep-generated
     passthru magento setup:di:compile
     task composer:autoload
