6f02e2a5ce6ee757f92c94251fe3b75789a6d819
Kieren Evans <kevans@inviqa.com>
2021-01-21 11:51:24 +0200

    Remove docker-sync support

--- src/_base/harness/scripts/disable.sh
+++ src/_base/harness/scripts/disable.sh
@@ -1,8 +1,6 @@
 #!/usr/bin/env bash
 
-if [[ "$USE_DOCKER_SYNC" = "yes" ]] && [ -f docker-sync.yml ]; then
-    run ws docker-sync stop
-elif [[ "$USE_MUTAGEN" = "yes" ]]; then
+if [[ "$USE_MUTAGEN" = "yes" ]]; then
     run ws mutagen pause
 fi
 
