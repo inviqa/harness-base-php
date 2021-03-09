efd6a8bc0215b8053bbf15ad1a864142aff4862f
Thomas Lobjoie <thomas.lobjoie@inviqa.com>
2021-03-09 14:55:00 +0000

    Add dump sanitised dump bash script to D8 skeleton.

---
+++ src/drupal8/application/skeleton/tools/database/dump_sanitised.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+curl --fail --silent --show-error --location --output /tmp/mtk-dump https://github.com/skpr/mtk/releases/download/v0.0.1/mtk-dump_linux_amd64
+chmod +x /tmp/mtk-dump
+
+if [ -L "$0" ] ; then
+    DIR="$(dirname "$(readlink -f "$0")")" ;
+else
+    DIR="$(dirname "$0")" ;
+fi
+
+BACKUP_TIME=$(date +"%Y-%m-%d--%H-%M-%S")
+BACKUP_FILE="/app/tools/assets/development/dump_sanitised.sql"
+
+/tmp/mtk-dump \
+--config=$DIR/mtk.config.yml \
+--host=$DB_HOST \
+--user=$DB_USER \
+--password=$DB_PASS \
+$DB_NAME > $BACKUP_FILE
