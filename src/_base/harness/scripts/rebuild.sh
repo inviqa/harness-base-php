16715f4290ac5f1cf7ea97d8f09f557c35f9aeb0
Andy Thompson <athompson@inviqa.com>
2021-03-16 17:10:43 +0000

    Remove console-build flag on rebuild

--- src/_base/harness/scripts/rebuild.sh
+++ src/_base/harness/scripts/rebuild.sh
@@ -4,6 +4,6 @@ run docker-compose down --rmi local --volumes --remove-orphans --timeout 120
 
 passthru ws cleanup built-images
 
-run rm -f .my127ws/.flag-built
+run rm -f .my127ws/.flag-built .my127ws/.flag-console-built
 
 passthru ws enable
