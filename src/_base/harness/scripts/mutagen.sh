33c027f59baacbbf25361b502d9b893db74ca267
Kieren Evans <kierenevans@users.noreply.github.com>
2021-04-01 16:49:52 +0300

    Clean up old mutagen sync sessions (#586)

--- src/_base/harness/scripts/mutagen.sh
+++ src/_base/harness/scripts/mutagen.sh
@@ -14,6 +14,10 @@ PATH="$PATH:./.my127ws/utilities/mutagen/"
 
 # shellcheck disable=SC2206
 CONTAINER_NAMES=($CONTAINER_NAMES)
+# shellcheck disable=SC2206
+SYNC_NAMES=($SYNC_NAMES)
+# shellcheck disable=SC2206
+FORWARD_NAMES=($FORWARD_NAMES)
 
 install_mutagen()
 {
@@ -62,13 +66,75 @@ start_mutagen_daemon()
     passthru mutagen daemon start
 }
 
+join_by_character()
+(
+  local IFS="$1"
+  shift
+  echo "$*"
+)
+
+clean_existing_projects()
+{
+    # Clean up the project if it's running
+    if mutagen project list > /dev/null 2>&1; then
+        passthru mutagen project terminate
+    fi
+
+    declare -a EXISTING_PROJECT_LABELS=()
+
+    local SYNC_NAME
+    local SYNC_LIST
+    for SYNC_NAME in ${SYNC_NAMES[*]}; do
+        # List syncs based on name
+        SYNC_LIST="$(mutagen sync list "$SYNC_NAME" 2> /dev/null || true)"
+        # Check if there are entries left
+        if [ "$(echo "$SYNC_LIST" | grep --count "URL: $(pwd)" | awk '{ print $1 }')" -gt 0 ]; then
+            # Build an array of sync session IDs to clean up
+            while IFS='' read -r line; do EXISTING_PROJECT_LABELS+=("$line"); done < <(echo "$SYNC_LIST" | grep -B4 "URL: $(pwd)" | grep io.mutagen.project | awk '{print $1"="$2}' | sed s/:=/=/)
+        fi
+    done
+
+    local FORWARD_NAME
+    local FORWARD_LIST
+    local CONTAINER_NAMES_REGEX
+    CONTAINER_NAMES_REGEX="$(join_by_character "\|" "${CONTAINER_NAMES[@]}")"
+    for FORWARD_NAME in ${FORWARD_NAMES[*]}; do
+        # List forwards based on name
+        FORWARD_LIST="$(mutagen forward list "$FORWARD_NAME" 2> /dev/null || true)"
+        # Check if there are entries left
+        if [ "$(echo "$FORWARD_LIST" | grep --count "URL: docker://\($CONTAINER_NAMES_REGEX\):tcp:" | awk '{ print $1 }')" -gt 0 ]; then
+            # Build an array of sync session IDs to clean up
+            while IFS='' read -r line; do EXISTING_PROJECT_LABELS+=("$line"); done < <(echo "$FORWARD_LIST" | grep -B6 "URL: docker://\($CONTAINER_NAMES_REGEX\):tcp:" | grep io.mutagen.project | awk '{print $1"="$2}' | sed s/:=/=/)
+        fi
+    done
+
+    if [ "${#EXISTING_PROJECT_LABELS[@]}" -gt 0 ]; then
+        echo "Found multiple mutagen sessions for this project."
+        echo "This can lead to increased CPU usage."
+        local REPLY=""
+        if [ -t 0 ] ; then
+          read -r -p 'Do you want to remove the other sessions? [Yes]/no: ' REPLY
+        fi
+        REPLY="${REPLY:-Yes}"
+        if ! [[ "$REPLY" =~ ^(Y|Yes|y|yes)$ ]]; then
+          return
+        fi
+
+        local LABEL_SELECTOR
+        for LABEL_SELECTOR in "${EXISTING_PROJECT_LABELS[@]}"; do
+          passthru mutagen sync terminate --label-selector "${LABEL_SELECTOR}"
+          passthru mutagen forward terminate --label-selector "${LABEL_SELECTOR}"
+        done
+    fi
+}
+
 start()
 {
     install_mutagen
     setup_sync_container
     start_mutagen_daemon
 
-    mutagen project list > /dev/null 2>&1 && passthru mutagen project terminate
+    clean_existing_projects
     passthru mutagen project start
     passthru mutagen project flush
 }
@@ -76,6 +142,7 @@ start()
 stop()
 {
     passthru mutagen project terminate
+    clean_existing_projects
     passthru docker rm -f "${CONTAINER_NAMES[@]}"
 }
 
