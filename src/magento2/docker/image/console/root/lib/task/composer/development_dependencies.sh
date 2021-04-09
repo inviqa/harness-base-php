ecfad075c4487fd121302731610dafd0e844cdd8
Kieren Evans <kierenevans@users.noreply.github.com>
2021-04-09 11:22:46 +0300

    Enable early feedback for magento2 in Jenkins (#590)

--- src/magento2/docker/image/console/root/lib/task/composer/development_dependencies.sh
+++ src/magento2/docker/image/console/root/lib/task/composer/development_dependencies.sh
@@ -4,7 +4,7 @@ function task_composer_development_dependencies()
 {
     passthru php -d opcache.file_cache_only=0 /usr/bin/composer install --no-interaction
     run rm -rf /tmp/php-file-cache/*/app/vendor/composer /tmp/php-file-cache/*/app/vendor/autoload.php* || true
-    passthru magento setup:upgrade --keep-generated
+    run mv app/etc/env.php app/etc/env-backup.php
     passthru magento setup:di:compile
     task composer:autoload
 }
---
+++ src/magento2/docker/image/console/root/lib/task/phpstan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function task_phpstan()
+{
+    local phpstan_version=""
+    phpstan_version="$(find_phpstan_version)"
+    echo "Using phpstan v$phpstan_version"
+    composer global require "phpstan/phpstan:$phpstan_version"
+    mv app/etc/env.php app/etc/env-backup.php
+    composer global exec -v -- phpstan analyse -c /app/phpstan.neon
+    mv app/etc/env-backup.php app/etc/env.php
+}
+
+function find_phpstan_version()
+(
+    local phpstan_version="0.12.37"
+    local project_phpstan_version=""
+    set +e
+    if [ -f /app/composer.lock ]; then
+        project_phpstan_version="$(jq -r '."packages" + ."packages-dev" | map(select( .name == "phpstan/phpstan" )) | .[].version' /app/composer.lock)"
+        if [ -n "$project_phpstan_version" ]; then
+          phpstan_version="$project_phpstan_version"
+        fi
+    fi
+    echo "$phpstan_version"
+)
