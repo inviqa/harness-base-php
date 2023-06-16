1178cdf9863918535ac470b16e1966ceaa48cce7
andytson-inviqa <andytson-inviqa@users.noreply.github.com>
2023-06-16 17:24:35 +0100

    Ensure getenv is only getting from environment variables (#778)

--- src/spryker/application/skeleton/config/Shared/config_local.php
+++ src/spryker/application/skeleton/config/Shared/config_local.php
@@ -34,9 +34,9 @@ use Twig\Cache\FilesystemCache;
 require 'common/config_oauth.php';
 
 $CURRENT_STORE = Store::getInstance()->getStoreName();
-$sprykerFrontendHost = getenv('YVES_HOST_' . $CURRENT_STORE);
-$sprykerBackendHost = getenv('ZED_HOST_' . $CURRENT_STORE);
-$sprykerBackendApiHost = getenv('ZED_API_HOST_' . $CURRENT_STORE);
+$sprykerFrontendHost = getenv('YVES_HOST_' . $CURRENT_STORE, true);
+$sprykerBackendHost = getenv('ZED_HOST_' . $CURRENT_STORE, true);
+$sprykerBackendApiHost = getenv('ZED_API_HOST_' . $CURRENT_STORE, true);
 
 $config[ConsoleConstants::ENABLE_DEVELOPMENT_CONSOLE_COMMANDS] = (bool)getenv('DEVELOPMENT_CONSOLE_COMMANDS', true);
 
@@ -74,8 +74,8 @@ $config[SessionConstants::ZED_SSL_ENABLED]
     = false;
 
 // ----------- Glue Application
-$config[GlueApplicationConstants::GLUE_APPLICATION_DOMAIN] = sprintf('http://%s', getenv('GLUE_HOST_' . $CURRENT_STORE));
-$config[GlueApplicationConstants::GLUE_APPLICATION_CORS_ALLOW_ORIGIN] = sprintf('http://%s', getenv('GLUE_HOST_' . $CURRENT_STORE));
+$config[GlueApplicationConstants::GLUE_APPLICATION_DOMAIN] = sprintf('http://%s', getenv('GLUE_HOST_' . $CURRENT_STORE, true));
+$config[GlueApplicationConstants::GLUE_APPLICATION_CORS_ALLOW_ORIGIN] = sprintf('http://%s', getenv('GLUE_HOST_' . $CURRENT_STORE, true));
 
 // ---------- Session
 $config[SessionConstants::YVES_SESSION_COOKIE_NAME] = $sprykerFrontendHost;
@@ -86,10 +86,10 @@ $config[SessionConstants::ZED_SESSION_COOKIE_NAME] = $sprykerBackendHost;
 $config[SessionConstants::ZED_SESSION_COOKIE_DOMAIN] = $sprykerBackendHost;
 
 // ---------- Database credentials
-$config[PropelConstants::ZED_DB_USERNAME] = getenv('DB_USER');
-$config[PropelConstants::ZED_DB_PASSWORD] = getenv('DB_PASS');
-$config[PropelConstants::ZED_DB_DATABASE] = getenv('DB_NAME');
-$config[PropelConstants::ZED_DB_HOST] = getenv('DB_HOST');
+$config[PropelConstants::ZED_DB_USERNAME] = getenv('DB_USER', true);
+$config[PropelConstants::ZED_DB_PASSWORD] = getenv('DB_PASS', true);
+$config[PropelConstants::ZED_DB_DATABASE] = getenv('DB_NAME', true);
+$config[PropelConstants::ZED_DB_HOST] = getenv('DB_HOST', true);
 $config[PropelConstants::ZED_DB_PORT] = 5432;
 $config[PropelConstants::USE_SUDO_TO_MANAGE_DATABASE] = false;
 $config[PropelConstants::ZED_DB_ENGINE]
@@ -97,93 +97,93 @@ $config[PropelConstants::ZED_DB_ENGINE]
     = PropelConfig::DB_ENGINE_PGSQL;
 
 // ---------- Elasticsearch
-$config[SearchElasticsearchConstants::HOST] = getenv('ELASTICSEARCH_HOST');
-$config[SearchElasticsearchConstants::TRANSPORT] = getenv('ELASTICSEARCH_SCHEME') ?: 'http';
-$config[SearchElasticsearchConstants::PORT] = getenv('ELASTICSEARCH_PORT');
-if (getenv('ELASTICSEARCH_USERNAME')) {
-    $config[SearchElasticsearchConstants::AUTH_HEADER] = base64_encode(getenv('ELASTICSEARCH_USERNAME') . ':' . getenv('ELASTICSEARCH_PASSWORD'));
+$config[SearchElasticsearchConstants::HOST] = getenv('ELASTICSEARCH_HOST', true);
+$config[SearchElasticsearchConstants::TRANSPORT] = getenv('ELASTICSEARCH_SCHEME', true) ?: 'http';
+$config[SearchElasticsearchConstants::PORT] = getenv('ELASTICSEARCH_PORT', true);
+if (getenv('ELASTICSEARCH_USERNAME', true)) {
+    $config[SearchElasticsearchConstants::AUTH_HEADER] = base64_encode(getenv('ELASTICSEARCH_USERNAME', true) . ':' . getenv('ELASTICSEARCH_PASSWORD', true));
 }
 $ELASTICA_INDEX_NAME = strtolower($CURRENT_STORE) . '_search';
 $config[CollectorConstants::ELASTICA_PARAMETER__INDEX_NAME] = $ELASTICA_INDEX_NAME;
 
 // ----------- Session and KV storage
-$config[StorageRedisConstants::STORAGE_REDIS_SCHEME] = getenv('REDIS_PROTOCOL');
-$config[StorageRedisConstants::STORAGE_REDIS_HOST] = getenv('REDIS_HOST');
-$config[StorageRedisConstants::STORAGE_REDIS_PORT] = getenv('REDIS_PORT');
-$config[StorageRedisConstants::STORAGE_REDIS_PASSWORD] = getenv('REDIS_PASSWORD');
+$config[StorageRedisConstants::STORAGE_REDIS_SCHEME] = getenv('REDIS_PROTOCOL', true);
+$config[StorageRedisConstants::STORAGE_REDIS_HOST] = getenv('REDIS_HOST', true);
+$config[StorageRedisConstants::STORAGE_REDIS_PORT] = getenv('REDIS_PORT', true);
+$config[StorageRedisConstants::STORAGE_REDIS_PASSWORD] = getenv('REDIS_PASSWORD', true);
 $config[StorageRedisConstants::STORAGE_REDIS_DATABASE] = 0;
-$config[SessionRedisConstants::YVES_SESSION_REDIS_SCHEME] = getenv('REDIS_PROTOCOL');
-$config[SessionRedisConstants::YVES_SESSION_REDIS_HOST] = getenv('REDIS_HOST');
-$config[SessionRedisConstants::YVES_SESSION_REDIS_PORT] = getenv('REDIS_PORT');
-$config[SessionRedisConstants::YVES_SESSION_REDIS_PASSWORD] = getenv('REDIS_PASSWORD');
+$config[SessionRedisConstants::YVES_SESSION_REDIS_SCHEME] = getenv('REDIS_PROTOCOL', true);
+$config[SessionRedisConstants::YVES_SESSION_REDIS_HOST] = getenv('REDIS_HOST', true);
+$config[SessionRedisConstants::YVES_SESSION_REDIS_PORT] = getenv('REDIS_PORT', true);
+$config[SessionRedisConstants::YVES_SESSION_REDIS_PASSWORD] = getenv('REDIS_PASSWORD', true);
 $config[SessionRedisConstants::YVES_SESSION_REDIS_DATABASE] = 1;
-$config[SessionRedisConstants::ZED_SESSION_REDIS_SCHEME] = getenv('REDIS_PROTOCOL');
-$config[SessionRedisConstants::ZED_SESSION_REDIS_HOST] = getenv('REDIS_HOST');
-$config[SessionRedisConstants::ZED_SESSION_REDIS_PORT] = getenv('REDIS_PORT');
-$config[SessionRedisConstants::ZED_SESSION_REDIS_PASSWORD] = getenv('REDIS_PASSWORD');
+$config[SessionRedisConstants::ZED_SESSION_REDIS_SCHEME] = getenv('REDIS_PROTOCOL', true);
+$config[SessionRedisConstants::ZED_SESSION_REDIS_HOST] = getenv('REDIS_HOST', true);
+$config[SessionRedisConstants::ZED_SESSION_REDIS_PORT] = getenv('REDIS_PORT', true);
+$config[SessionRedisConstants::ZED_SESSION_REDIS_PASSWORD] = getenv('REDIS_PASSWORD', true);
 $config[SessionRedisConstants::ZED_SESSION_REDIS_DATABASE] = 2;
 
 // >>> SECURITY BLOCKER
-$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_SCHEME] = getenv('REDIS_PROTOCOL');
-$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_HOST] = getenv('REDIS_HOST');
-$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_PORT] = getenv('REDIS_PORT');
-$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_PASSWORD] = getenv('REDIS_PASSWORD');
+$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_SCHEME] = getenv('REDIS_PROTOCOL', true);
+$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_HOST] = getenv('REDIS_HOST', true);
+$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_PORT] = getenv('REDIS_PORT', true);
+$config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_PASSWORD] = getenv('REDIS_PASSWORD', true);
 $config[SecurityBlockerConstants::SECURITY_BLOCKER_REDIS_DATABASE] = 3;
 
 // ---------- RabbitMq
-$config[RabbitMqEnv::RABBITMQ_API_HOST] = getenv('RABBITMQ_HOST');
-$config[RabbitMqEnv::RABBITMQ_API_PORT] = getenv('RABBITMQ_API_PORT');
-$config[RabbitMqEnv::RABBITMQ_API_USERNAME] = getenv('RABBITMQ_USER');
-$config[RabbitMqEnv::RABBITMQ_API_PASSWORD] = getenv('RABBITMQ_PASSWORD');
+$config[RabbitMqEnv::RABBITMQ_API_HOST] = getenv('RABBITMQ_HOST', true);
+$config[RabbitMqEnv::RABBITMQ_API_PORT] = getenv('RABBITMQ_API_PORT', true);
+$config[RabbitMqEnv::RABBITMQ_API_USERNAME] = getenv('RABBITMQ_USER', true);
+$config[RabbitMqEnv::RABBITMQ_API_PASSWORD] = getenv('RABBITMQ_PASSWORD', true);
 $config[RabbitMqEnv::RABBITMQ_CONNECTIONS] = [
     'DE' => [
         RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'DE-connection',
-        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
-        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
-        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
-        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
-        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST_DE'),
+        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST', true),
+        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT', true),
+        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD', true),
+        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER', true),
+        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST_DE', true),
         RabbitMqEnv::RABBITMQ_STORE_NAMES => ['DE'],
     ],
     'AT' => [
         RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'AT-connection',
-        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
-        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
-        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
-        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
-        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST_AT'),
+        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST', true),
+        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT', true),
+        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD', true),
+        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER', true),
+        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST_AT', true),
         RabbitMqEnv::RABBITMQ_STORE_NAMES => ['AT'],
     ],
     'US' => [
         RabbitMqEnv::RABBITMQ_CONNECTION_NAME => 'US-connection',
-        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST'),
-        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT'),
-        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD'),
-        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER'),
-        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST_US'),
+        RabbitMqEnv::RABBITMQ_HOST => getenv('RABBITMQ_HOST', true),
+        RabbitMqEnv::RABBITMQ_PORT => getenv('RABBITMQ_PORT', true),
+        RabbitMqEnv::RABBITMQ_PASSWORD => getenv('RABBITMQ_PASSWORD', true),
+        RabbitMqEnv::RABBITMQ_USERNAME => getenv('RABBITMQ_USER', true),
+        RabbitMqEnv::RABBITMQ_VIRTUAL_HOST => getenv('RABBITMQ_VHOST_US', true),
         RabbitMqEnv::RABBITMQ_STORE_NAMES => ['US'],
     ],
 ];
-$config[RabbitMqEnv::RABBITMQ_API_VIRTUAL_HOST] = getenv('RABBITMQ_VHOST_' . $CURRENT_STORE);
+$config[RabbitMqEnv::RABBITMQ_API_VIRTUAL_HOST] = getenv('RABBITMQ_VHOST_' . $CURRENT_STORE, true);
 $config[RabbitMqEnv::RABBITMQ_CONNECTIONS][$CURRENT_STORE][RabbitMqEnv::RABBITMQ_DEFAULT_CONNECTION] = true;
 
 // ---------- Scheduler
 $config[SchedulerConstants::ENABLED_SCHEDULERS] = [];
 
-if ((getenv('HAS_JENKINS_RUNNER') ?: 'true') === 'true') {
+if ((getenv('HAS_JENKINS_RUNNER', true) ?: 'true') === 'true') {
     $config[SchedulerConstants::ENABLED_SCHEDULERS] = [
         SchedulerConfig::PYZ_SCHEDULER_JENKINS,
     ];
     $config[SchedulerJenkinsConstants::JENKINS_CONFIGURATION] = [
         SchedulerConfig::PYZ_SCHEDULER_JENKINS => [
-            SchedulerJenkinsConfig::SCHEDULER_JENKINS_BASE_URL => 'http://' . getenv('JENKINS_HOST') . ':' . getenv('JENKINS_PORT') . '/',
+            SchedulerJenkinsConfig::SCHEDULER_JENKINS_BASE_URL => 'http://' . getenv('JENKINS_HOST', true) . ':' . getenv('JENKINS_PORT', true) . '/',
         ],
     ];
 }
 
 // ---------- Mail configuration
-$config[MailConstants::SMTP_HOST] = getenv('SMTP_HOST');
-$config[MailConstants::SMTP_PORT] = getenv('SMTP_PORT');
+$config[MailConstants::SMTP_HOST] = getenv('SMTP_HOST', true);
+$config[MailConstants::SMTP_PORT] = getenv('SMTP_PORT', true);
 
 // ---------- Setting to work with queue process timeout patch
 $config[QueueConstants::QUEUE_PROCESS_TIMEOUT] = 120;
